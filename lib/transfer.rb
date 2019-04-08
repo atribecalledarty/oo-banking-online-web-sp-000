cd42e4c82a2c8b78fcb0e983b950c28faf8bd57e
Arthur Song <arth3rs0ng@gmail.com>
2019-04-08 01:24:09 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -15,10 +15,12 @@ class Transfer
   end
   
   def execute_transaction
-    if @sender.valid? && @status == "pending"
+    if valid? && @status == "pending"
       @sender.balance -= @amount
       @receiver.balance += @amount
       @status = "complete"
+    else
+      "Transaction rejected. Please check your account balance."
     end
   end
 end
