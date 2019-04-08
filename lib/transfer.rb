fabc7f692c1eb862b6f00934dfc834df5a55f47e
Arthur Song <arth3rs0ng@gmail.com>
2019-04-08 01:20:21 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,10 @@
 class Transfer
   # your code here
+  attr_reader :sender, :receiver, :amount
+  
+  def initialize(sender, receiver, amount)
+    @sender = sender
+    @receiver = receiver
+    @amount = amount
+  end
 end
